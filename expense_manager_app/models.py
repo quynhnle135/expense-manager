43a6e9084943c6e5d83a6020bacbf29b7dc62cf0
Quinn Le <quynhnle135@gmail.com>
2023-12-01 10:31:18 -0500

    Set up Expense model and project settings

--- expense_manager_app/models.py
+++ expense_manager_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    card = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    expense_date = models.DateField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+    notes = models.TextField(null=True, blank=True)
+
--- expense_manager_app/views.py
+++ expense_manager_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Welcome to the expense manager")
